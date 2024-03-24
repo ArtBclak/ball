e9088124f9f18aac9c3a501c6b2dd13d9bc0d810
ArtBclak <0960469634kvstrt@gmail.com>
2024-03-24 19:59:13 +0100

    1

---
+++ index.js
@@ -0,0 +1,19 @@
+// const ball = document.querySelector(".ball")
+
+
+
+document.addEventListener('keydown', function(e) {
+    const k = e.key 
+    if(k === "ArrowUp" || k === "w") {
+        console.log(k)
+    } 
+    if(k === "ArrowDown" || k === "s") {
+
+    } 
+    if(k === "ArrowLeft" || k === "a") {
+
+    } 
+    if(k === "ArrowRight" || k === "d") {
+
+    } 
+  });
